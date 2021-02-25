47eced5693c85c1651c80d5dd8573fb9dd920d9a
jojo-tey <dosagam6301@gmail.com>
2021-02-25 02:41:53 +0100

    new settings

--- djgram/djgram/settings.py
+++ djgram/djgram/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-# import my_settings
+import my_settings
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -89,12 +90,7 @@ WSGI_APPLICATION = 'djgram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = my_settings.default
 
 
 # Password validation
@@ -148,14 +144,22 @@ STATICFILES_DIRS = [
 
 # user-contents upload path
 MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_ROOT = '/vol/web/media'
+# for dev
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# for deploy
+# MEDIA_ROOT = '/vol/web/media'
+
+# Email-Backend
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 # INTERNAL_IPS = ['127.0.0.1']
 
 # 데코레이터로 로그인 확인을 달았기때문에 로그인 상태가 아닐 시 리다이렉트 시켜줌
 LOGIN_URL = '/login/'
 
+# LOGIN_REDIRECT_URL = 'index'
+# LOGOUT_REDIRECT_URL = 'login'
 
 # setting.py 파일을 개발용과 배포용으로 나눌때 -
 # aws 관련 / DB관련 등 필요한것 따로 빼서 dev.py 등으로 만든 후
--- djgram/djgram/urls.py
+++ djgram/djgram/urls.py
@@ -45,13 +45,14 @@ urlpatterns = [
     path('relation/', RelationView.as_view(), name='contents_relation'),
 ]
 
+# 로컬 테스트용 스토리지 - 배포시 필요없음 - 배포시 스태틱 경로도 수정해줘야함
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
+
+
 # DEBUG_TOOLBAR has issue in django 3.1 // solving
 
 
 # if settings.DEBUG:
 #     import debug_toolbar
 #     urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
-
-# 로컬 테스트용 스토리지 - 배포시 필요없음 - 배포시 스태틱 경로도 수정해줘야함
-urlpatterns += static(settings.MEDIA_URL,
-                      document_root=settings.MEDIA_ROOT)
